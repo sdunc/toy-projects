fa4e6a5a2f8e362405c2fedea78caad7c61c7fc2
Stephen Duncanson <Stephen.duncanson@gmail.com>
2021-09-26 16:31:59 -0400

    added comments, cleaned up

--- art.py
+++ art.py
@@ -1,73 +1,66 @@
+# a dirty hack to make e-art
+
 from PIL import Image
 
-def linear_sample(start=0,stop=10,n=10):
-        # return n samples between start and stop
-        # return in an array
-        samples = []
-        step_size = (stop-start)//n # integer div step size
-        for i in range(start+step_size, stop, step_size):
-                samples.append(i)
-        return samples
+def sample(start,stop,n):
+    samples = []
+    step_size = (stop-start)//n # integer div step size
+    for i in range(start+step_size, stop, step_size):
+        samples.append(i)
+    return samples
 
-im = Image.open("Henri.jpg")
-xsize, ysize = im.size
-print(xsize, ysize, im.mode)
-# define a border around the image
-top_bottom_padding = int(0.08 * ysize) # 10% for top and bottom
-right_left_padding = int(0.15 * xsize) # 20% for right and left
-print(top_bottom_padding, right_left_padding)
+# open an image from the working dir and get dims
+original_image = Image.open("Henri.jpg")
+xsize, ysize = original_image.size
 
-square_size = int(0.03*xsize) # 5% of the bigger edge is square size
-spacing_y = int(0.05*max(xsize,ysize)) # 5% of the bigger edge is square size
-spacing_x = int(0.05*max(xsize,ysize)) # 5% of the bigger edge is square size
+# define some padding arounnd the image where we won't cut 
+top_bottom_padding = int(0.08 * ysize)
+right_left_padding = int(0.15 * xsize)
 
-# The region is defined by a 4-tuple, where coordinates are (left, upper, right, lower). 
-# The Python Imaging Library uses a coordinate system with (0, 0) in the upper left corner. 
-# Also note that coordinates refer to positions between the pixels, 
-# so the region in the above example is exactly 300x300 pixels.
+# define how large we want the squares to be and the spacing between them
+square_size = int(0.03*xsize) # 5% of the width is a good size? Do whatever.
+spacing_y = int(0.05*max(xsize,ysize)) # 5% of the bigger edge is square size?
+spacing_x = int(0.05*max(xsize,ysize)) # 5% of the bigger edge is square size?
 
-# iterate over the image row by row adding to our position until we are at the 
-# the other edge
-
-# start in the top left corner 
-current_x = right_left_padding
-current_y = top_bottom_padding
-
+# how many squares do we want in x direction (rows) and y (cols)
 xn = 10
 yn = 15
 
-x_points = linear_sample(start=right_left_padding, stop=int(xsize-right_left_padding-square_size), n=xn)
-y_points = linear_sample(start=top_bottom_padding, stop=int(ysize-top_bottom_padding-square_size), n=yn) # eventually do n for x and y with img res
+# get the px coords we want to cut squares out in x and y directions
+# when iterated over in nested for loops this will make a grid
+x_points = sample(start=right_left_padding, stop=int(xsize-right_left_padding-square_size), n=xn)
+y_points = sample(start=top_bottom_padding, stop=int(ysize-top_bottom_padding-square_size), n=yn) 
 
+# initialize a new RGB image of the proper size
 new_image = Image.new("RGB",(square_size*xn, square_size*yn))
 
-# we paste back in to a small image
+# these are the coordinates in the new image (above)
+# we will paste the rectangles cut from old image onto these coords
 newy = 0
 newx = 0
-# these are added two with every crop
 
+# create a white square image to paste onto original image
 white_square = Image.new('RGB', (square_size, square_size), (256, 256, 256))
+
+# NOTE ON THESE TUPLES
+# The region is defined by a 4-tuple, where coordinates are (left, upper, right, lower). 
+# The Python Imaging Library uses a coordinate system with (0, 0) in the upper left corner. 
+# Also note that coordinates refer to positions between the pixels, 
 
 for y in y_points:
         for x in x_points:
-                print(x,y)
-                box=(x,y,x+square_size,y+square_size)
-                region = im.crop(box)
-                #region = region.transpose(Image.ROTATE_90)
-
-                im.paste(white_square,box)
-
-                new_box=(newx,newy,newx+square_size,newy+square_size)
-                new_image.paste(region,new_box)
-                newx = (newx + square_size) % new_image.size[0]
-        newy = (newy+square_size) % new_image.size[1] # make sure it fits
-        
-                #         break
-        #         newx+=square_size
-        #         print(newx,newy)
-        # newy+=square_size
-        # print(newx,newy)
-
+            print(x,y)
+            box=(x,y,x+square_size,y+square_size)
+            region = im.crop(box)
+            original_image.paste(white_square,box)
+            new_box=(newx,newy,newx+square_size,newy+square_size)
+            new_image.paste(region,new_box)
+            newx = (newx + square_size) % new_image.size[0]
+            newy = (newy+square_size) % new_image.size[1] # make sure it fits
+                
+# show both images, your OS should have a default image viewer
 im.show()
 new_image.show()
+
+
 
