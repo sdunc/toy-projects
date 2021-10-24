b56dc53023193ec2adcda856c974fe05325875b5
Stephen Duncanson <Stephen.duncanson@gmail.com>
2021-10-24 14:03:57 -0400

    Add files via upload

---
+++ exam_notes.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+  // COMMAND LINE ARGUMENTS
+  // int argc - number of arguments
+  // char* argv[] - array of strings for each arg. argv[0] bin name
+  printf("You passed in %d command line arguments\n",argc-1);
+
+
+  
+
+
+
+
+
+
+
+  
+}
